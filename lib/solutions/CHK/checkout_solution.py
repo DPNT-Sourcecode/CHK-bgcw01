35ae360f1a65bbeaa33eef156f5c452984f2f94c
sbx_user1051 <sbx_user1051@169.254.62.33>
2026-01-15 18:05:19 +0000

    Thu Jan 15 18:05:19 UTC 2026

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,33 @@
 
 class CheckoutSolution:
-    def computeDiscount(self, itemNum, offer, offerPrice, regPrice):
-        numOfDiscoun = (items_counts["A"]) // offer
+
+
+    def computeTotal(self, item, itemNumber, offerPrice, regPrice):
+        prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+        discounts = {"A": True, "B": True, "C": False, "D": False}
+        amountForDiscount = {"A": 3, "B": 2, "C": 0, "D": 0}
+        offerPrice = {"A": 130, "B": 45, "C": 0, "D": 0}
+        total = 0
+        discountItems = 0
+
+        #Compute discount if any
+        if discounts[item] == True:
+            if item == "A":
+                discountItems = (itemNumber["A"]) // amountForDiscount["A"]
+                itemNumber =- discountItems
+
+            elif item == "B":
+        else:
+            total = prices[item] * itemNumber
+
+        return total
+
+
     # skus = unicode string
 
     def checkout(self, skus):
         items = list(skus)
+
         #count number of items
         item_counts = {}
         for i in items:
@@ -15,7 +37,9 @@ class CheckoutSolution:
                 item_counts[i] = 1
 
         #total depending on number and sales
-        computeDiscount()
+        #for length of dict
+        computeTotal()
+
 
 
 
