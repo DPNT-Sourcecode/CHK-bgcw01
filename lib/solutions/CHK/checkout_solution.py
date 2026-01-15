b1a1c216f1f3f333b3aa8e77710875bb90a5aeb6
sbx_user1051 <sbx_user1051@169.254.62.33>
2026-01-15 17:38:19 +0000

    Thu Jan 15 17:38:19 UTC 2026

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,19 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         items = list(skus)
-        
+        #count number of items
+        item_counts = {}
+        for i in items:
+            if i in  item_counts:
+                item_counts[i] +=1
+            else:
+                item_counts[i] = 1
+
+        #total depending on number and sales
+        for i in item_counts:
+            if 
+
+
+
 
 
