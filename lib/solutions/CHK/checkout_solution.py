2751b93fa13aa21532e1e4cd2d61f3893ef60f0d
sbx_user1051 <sbx_user1051@169.254.62.33>
2026-01-15 18:17:19 +0000

    Thu Jan 15 18:17:19 UTC 2026

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,27 @@
-
+import pytest
 class CheckoutSolution:
+
+    def computeTotal(self, item, itemNumber):
+        prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+        discounts = {"A": True, "B": True, "C": False, "D": False}
+        amountForDiscount = {"A": 3, "B": 2, "C": 0, "D": 0}
+        offerPrice = {"A": 130, "B": 45, "C": 0, "D": 0}
+        runningTotal = 0
+        discountItems = 0
+
+        #Compute discount if any per item
+        if discounts[item] == True:
+            discountItems = (itemNumber[item]) // amountForDiscount[item]
+            itemNumber -= discountItems
+            runningTotal += offerPrice[item] * discountItems
+
+        runningTotal += prices[item] * itemNumber
+
+        return runningTotal
+
+
     # skus = unicode string
+
     def checkout(self, skus):
         items = list(skus)
 
@@ -12,28 +33,13 @@ class CheckoutSolution:
             else:
                 item_counts[i] = 1
         total = 0
-
         #total depending on number and sales
         #for length of dict
         for item in item_counts:
-            total += computeTotal(item, item_counts[item])
+            total += self.computeTotal(item, item_counts[item])
 
-    def computeTotal(item, itemNumber):
-        prices = {"A": 50, "B": 30, "C": 20, "D": 15}
-        discounts = {"A": True, "B": True, "C": False, "D": False}
-        amountForDiscount = {"A": 3, "B": 2, "C": 0, "D": 0}
-        offerPrice = {"A": 130, "B": 45, "C": 0, "D": 0}
-        runningTotal = 0
-        discountItems = 0
 
-        # Compute discount if any per item
-        if discounts[item] == True:
-            discountItems = (itemNumber[item]) // amountForDiscount[item]
-            itemNumber -= discountItems
-            runningTotal += offerPrice[item] * discountItems
+assert 
 
-        runningTotal += prices[item] * itemNumber
-
-        return runningTotal
 
 
